2bc9ba09236005476f9b318a4bb6c40e72cf1144
Dalton Felipe <daltonphellipe@gmail.com>
2018-04-05 09:16:54 -0300

    update 05/04/18

--- msgs.py
+++ msgs.py
@@ -8,15 +8,20 @@ Comandos aceitáveis
 /help - Exibe o menu ajuda
 
 /math - Recebe uma expressão matemática e envia sua solução.
- - Ex.: /math sin(1+1) - (seno de 1 + 1)
- - /math_lista - lista das operações possiveis.
+>>> Ex.: /math sin(1+1) - (seno de 1 + 1)
+>>> /math_lista - lista das operações possiveis.
 
 /integral - Calcula a integral de uma função.
-	Ex.: /integral cos(2*x),x - (integral de cos(2x) em relação a x)
+>>> Ex.: /integral cos(2*x),x - (integral de cos(2x) em relação a x)
 
 /dx - Calcula a derivada de uma uma função.
-	Ex.: /dx x**2,x - (derivada de x**2 em relação a x)
+>>> Ex.: /dx x**2,x - (derivada de x**2 em relação a x)
 
+/grafico - Gera um grafico bidimensional com a função desejada.
+>>> Ex.: /grafico x**2
+
+/grafico3d - Gera um grafico tridimensional com a função desejada.
+>>> Ex.: /grafico3d x**2 + y**2
 
 *  Os comandos aceitos são das libs numpy e sympy do  python2.7.
 ** Outros comandos serão ignorados e não terão resposta.
